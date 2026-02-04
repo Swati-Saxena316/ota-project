01e41ae193597eccd012f95262a1bd55f83761b8
Swati-Saxena316 <Swatisaxena5104@gmail.com>
2026-02-04 15:03:51 +0530

    Update manifest_client.h

--- ota project/manifest/manifest_client.h
+++ ota project/manifest/manifest_client.h
@@ -9,7 +9,7 @@ typedef struct {
     char url[256];
     char sha256[65];          // hex string (64 chars + null)
     size_t size_bytes;
-    char release_notes[256];  // optional
+    char release_notes[256];
 } ota_manifest_t;
 
 // Fetches and parses OTA manifest from OTA_MANIFEST_URL
